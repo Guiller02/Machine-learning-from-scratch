2b36194ddec62c7798af3934bf1c9e8bd777cb4b
Guiller02 <guilhermepeixoto27@gmail.com>
2020-01-23 14:08:48 -0300

    adding KNN algorithm

--- Linear-regression/regression_slope.py
+++ Linear-regression/regression_slope.py
@@ -2,13 +2,26 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
 #data
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+# xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+# ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
+def create_dataset(hm,variance,step=2,correlation=False):
+    val=1
+    ys = []
+    for i in range(hm):
+        y = val+random.randrange(-variance,variance)
+        ys.append(y)
+        if correlation and correlation =='pos':
+            val+=step
+        elif correlation and correlation =='neg':
+            val-=step
+        xs=[i for i in range(len(ys))]
+    return np.array(xs,dtype=np.float64), np.array(ys,dtype=np.float64)
 def best_fit_slope_and_intercept(xs,ys):
     # media(x) * media(y) - media(x*y)
     # ________________________________
@@ -32,6 +45,8 @@ def coeficient_of_determination(ys_orig,ys_line):
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr/squared_error_y_mean)
 
+xs, ys = create_dataset(40,10,2,correlation='pos')
+
 m,b = best_fit_slope_and_intercept(xs,ys)
 
 # y = mx + b
@@ -44,6 +59,6 @@ r_squared = coeficient_of_determination(ys,regression_line)
 print(r_squared)
 
 plt.scatter(xs,ys)
-plt.scatter(predict_x,predict_y, color='green')
 plt.plot(xs,regression_line)
+plt.scatter(predict_x,predict_y, color='green')
 plt.show()
